65a59a152fdc6e0ad79c51b38bc6e5f3e516bf28
Sermad Ahmad <sermadahmad024@gmail.com>
2025-01-12 22:06:34 +0500

    feat: getrandomcolor
    an app having a background color changed on button press and a checkbox to continuously change color

--- App.tsx
+++ App.tsx
@@ -1,12 +1,56 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Button, StyleSheet, Text, View} from 'react-native';
+import BouncyCheckbox from 'react-native-bouncy-checkbox';
+
+const getRandomColor = () => {
+  const getRandomHex = () => Math.floor(Math.random() * 16).toString(16);
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += getRandomHex();
+  }
+  return color;
+}
 
 const App = () => {
+  const [isChecked, setIsChecked] = React.useState(false);
+  const [bgclr, setBgclr] = React.useState('lightblue');
+
+  if (isChecked) {
+    setTimeout(() => {
+      setBgclr(getRandomColor())
+    }, 300)
+  }
+  
   return (
-    <View>
-      <Text>React Native</Text>
+    <View style={[styles.container, {backgroundColor: bgclr}]}>
+      <View>
+        <Button title="Change Color" onPress={() => setBgclr(getRandomColor())} />
+        <View style={styles.checkbox}>
+          <BouncyCheckbox 
+            fillColor='firebrick'
+            isChecked={isChecked}
+            onPress={() => setIsChecked(!isChecked)}
+            textComponent={
+              <Text style={{padding:10, fontWeight:'bold' }}>{
+                isChecked ? 'Turn off Disco Mode' : 'Turn on Disco Mode'
+              }</Text>
+            }
+          />
+        </View>
+      </View>
     </View>
   );
 };
 
 export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  checkbox: {
+    
+  },
+});
