390b3998c701f642c59a03dd8422302168770678
Sermad Ahmad <sermadahmad024@gmail.com>
2025-01-13 21:46:48 +0500

    working on toggle visibility task

--- App.tsx
+++ App.tsx
@@ -1,12 +1,47 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Button, StyleSheet, Text, View} from 'react-native';
 
 const App = () => {
+  const [isVisible, setIsVisible] = React.useState(false);
+  const [toggleCount, setToggleCount] = React.useState(0);
   return (
-    <View>
-      <Text>React Native</Text>
+    <View style={styles.container}>
+      {!isVisible && (
+        <Text style={styles.txt}>
+          I am a Toggle able Text. Try Toggling me.
+        </Text>
+      )}
+      <Button
+        title={isVisible ? 'Show Text' : 'Hide Text'}
+        onPress={() => {
+          setIsVisible(!isVisible);
+          setToggleCount(toggleCount + 1);
+        }}
+      />
+      <Text style={styles.heading}>Toggle Logs: </Text>
+      <Text style={styles.txt}>Toggle Count: {toggleCount}</Text>
     </View>
   );
 };
 
 export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: '#fff7e6',
+  },
+  txt: {
+    fontSize: 20,
+    color: 'red',
+    padding: 10,
+  },
+  heading: {
+    fontSize: 24,
+    color: 'green',
+    padding: 10,
+    fontWeight: 'bold',
+  },
+
+});
